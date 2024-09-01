d1bf8d02f467818dcaf5b6ed76f753bde759af6b
unknown <vedataliu1@gmail.com>
2024-09-01 08:28:18 +0200

    Project organization structure back end - Data

---
+++ Wilki/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Wilki;
+using Wilki.Models;
+using Wilki.Models.MbrojtjaEProjektit;
+
+namespace Wilki.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Perdoruesi> Perdoruesit { get; set; }
+        public DbSet<TeDhenatPerdoruesit> TeDhenatPerdoruesit { get; set; }
+        public DbSet<SliderOfertat> SliderOfertat { get; set; }
+        public DbSet<Shporta> Shporta { get; set; }
+        public DbSet<KategoriaProduktit> KategoriaProduktit { get; set; }
+        public DbSet<KompanitePartnere> KompanitePartnere { get; set; }
+        public DbSet<AdresatPerdoruesit> AdresatPerdoruesit { get; set; }
+        public DbSet<Produkti> Produkti { get; set; }
+        public DbSet<TeDhenatProduktit> TeDhenatProduktit { get; set; }
+        public DbSet<TeDhenatShporta> TeDhenatShporta { get; set; }
+        public DbSet<ContactForm> ContactForm { get; set; }
+        public DbSet<KodiZbritjes> KodiZbritjes { get; set; }
+        public DbSet<Porosit> Porosit { get; set; }
+        public DbSet<TeDhenatEPorosis> TeDhenatEPorosis { get; set; }
+        public DbSet<TeDhenatBiznesit> TeDhenatBiznesit { get; set; }
+        public DbSet<Bankat> Bankat { get; set; } 
+        public DbSet<ListaEDeshirave> ListaEDeshirave {  get; set; }
+        public DbSet<ZbritjaQmimitProduktit> ZbritjaQmimitProduktit { get; set; }
+        public DbSet<AdminLogs> AdminLogs { get; set; }
+        public DbSet<Building> Building { get; set; }
+        public DbSet<Renovation> Renovation { get; set; }
+    }
+}
