62f8e902b40f640d9f7029e26394395ea04a6d88
ahmetb00 <121351827+ahmetb00@users.noreply.github.com>
2024-09-03 12:25:56 +0200

    Creating models for "Bankat", "KodiZbritjes" and creating the controller "Home"

---
+++ Wilki/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Wilki.Models;
+using Microsoft.AspNetCore.Authorization;
+using Wilki.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wilki.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+                return LocalRedirect("/Identity/Account/Login");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
