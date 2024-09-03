30202d579ee3071804f120539b3addcb3910eb27
Lekkelmendi7 <lek.kelmendi8@gmail.com>
2024-09-03 13:33:43 +0200

    Creating controllers for "AdminLogs " and ContactForm

---
+++ Wilki/Controllers/API/TeNdryshme/AdminLogsController.cs
@@ -0,0 +1,31 @@
+﻿using Wilki.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wilki.Controllers.API.TeNdryshme
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Route("api/TeNryshme/[controller]")]
+    [ApiController]
+    public class AdminLogsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminLogsController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("ShfaqGjurmimet")]
+        public async Task<IActionResult> ShfaqGjurmimet()
+        {
+            var gjurmimet = await _context.AdminLogs.Include(x => x.Stafi).ToListAsync();
+
+            return Ok(gjurmimet);
+        }
+    }
+}
---
+++ Wilki/Controllers/API/TeNdryshme/ContactFormController.cs
@@ -0,0 +1,136 @@
+﻿using Wilki.Data;
+using Wilki.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wilki.Controllers.API.TeNdryshme
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Route("api/TeNdryshme/[controller]")]
+    [ApiController]
+    public class ContactFormController : ControllerBase
+    {
+        public readonly ApplicationDbContext _context;
+
+        public ContactFormController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin, Shites")]
+        [HttpGet]
+        [Route("shfaqMesazhet")]
+        public async Task<IActionResult> ShfaqMesazhet()
+        {
+            List<ContactForm> ContactForm = await _context.ContactForm
+                .Include(x => x.User)
+                .OrderByDescending(x => x.MesazhiId)
+                .ToListAsync();
+
+            var mesazhet = ContactForm.Select(x => new
+            {
+                x.Emri,
+                x.Email,
+                x.UserId,
+                x.Statusi,
+                x.Mesazhi,
+                x.DataDergeses,
+                x.MesazhiId,
+                User = x.User != null ? new
+                {
+                    x.User.UserID,
+                    x.User.Email
+                } : null
+            });
+
+            return Ok(ContactForm);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("shfaqMesazhetNgaUseri")]
+        public async Task<IActionResult> GetMesazhetUserit(int idUserit)
+        {
+            List<ContactForm> ContactForm = await _context.ContactForm
+                .Include(x => x.User)
+                .OrderByDescending(x => x.MesazhiId)
+                .Where(x => x.UserId == idUserit)
+                .ToListAsync();
+
+            var mesazhet = ContactForm.Select(x => new
+            {
+                x.Emri,
+                x.Email,
+                x.UserId,
+                x.Statusi,
+                x.Mesazhi,
+                x.DataDergeses,
+                x.MesazhiId,
+                User = x.User != null ? new
+                {
+                    x.User.UserID,
+                    x.User.Email
+                } : null
+            });
+
+            return Ok(mesazhet);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("shfaqMesazhinSipasIDs")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var msg = await _context.ContactForm.FirstOrDefaultAsync(x => x.MesazhiId == id);
+
+            return Ok(msg);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("shtoMesazhin")]
+        public async Task<IActionResult> Post(ContactForm contactform)
+        {
+            await _context.ContactForm.AddAsync(contactform);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Get", contactform.MesazhiId, contactform);
+
+        }
+
+        [Authorize(Roles = "Admin, Shites")]
+        [HttpPut]
+        [Route("konfirmoMesazhin")]
+        public async Task<IActionResult> Put(int id, [FromBody] ContactForm m)
+        {
+            var mesazhi = await _context.ContactForm.FirstOrDefaultAsync(x => x.MesazhiId == id);
+            if (mesazhi == null)
+            {
+                return BadRequest("Nuk ka asnje mesazhe me kete ID");
+            }
+            mesazhi.Statusi = "Mesazhi eshte Pranuar dhe eshte Pergjigjur ne email";
+            _context.ContactForm.Update(mesazhi);
+            await _context.SaveChangesAsync();
+
+
+            return Ok(mesazhi);
+        }
+
+        [Authorize(Roles = "Admin, Shites")]
+        [HttpDelete]
+        [Route("fshiMesazhin")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var mesazhi = await _context.ContactForm.FirstOrDefaultAsync(x => x.MesazhiId == id);
+
+            if (mesazhi == null)
+                return BadRequest("Invalid id");
+
+            _context.ContactForm.Remove(mesazhi);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
