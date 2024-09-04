394b02baa499d4cec8205872eef07ade4dde6e44
eriDev1 <99198496+eriDev1@users.noreply.github.com>
2024-09-04 16:43:32 +0200

    Created models for the product and the bag & Slider offers

---
+++ Wilki/Models/Produkti.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wilki.Models
+{
+    public class Produkti
+    {
+        [Key]
+        public int ProduktiId { get; set; }
+
+        public string? EmriProduktit { get; set; } = "";
+
+        public string? Pershkrimi { get; set; } = "";
+
+        public string? FotoProduktit { get; set; } = "ProduktPaFoto.png";
+
+        public int? KompaniaId { get; set; }
+
+        public int? KategoriaId { get; set; }
+        public string? isDeleted { get; set; } = "false";
+
+        [ForeignKey(nameof(KategoriaId))]
+        public virtual KategoriaProduktit? Kategoria { get; set; }
+
+        [ForeignKey(nameof(KompaniaId))]
+        public virtual KompanitePartnere? KompanitePartnere { get; set; }
+
+        public virtual TeDhenatProduktit? TeDhenatProduktit { get; set; }
+
+        public virtual List<TeDhenatEPorosis>? TeDhenatEPorosis { get; set; }
+        public virtual ZbritjaQmimitProduktit? ZbritjaQmimitProduktit { get; set; }
+    }
+}
---
+++ Wilki/Models/Shporta.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wilki.Models
+{
+    public class Shporta
+    {
+        [Key]
+        public int ShportaID { get; set; }
+        public int? PerdoruesiID { get; set; }
+        public int? AdresaPorosisID { get; set; }
+        public string? KodiZbritjesID { get; set; } = "NukKaZbritje";
+        public int? TotaliProdukteveNeShporte { get; set; } = 0;
+        public decimal? Totali18TVSH { get; set; } = 0;
+        public int? TotProd18TVSH { get; set; } = 0;
+        public decimal? Totali8TVSH { get; set; } = 0;
+        public int? TotProd8TVSH { get; set; } = 0;
+        public DateTime? DataEFunditEPerditesimit { get; set; } = DateTime.Now;
+        [ForeignKey(nameof(PerdoruesiID))]
+        public virtual Perdoruesi? Perdoruesi { get; set;}
+        [ForeignKey(nameof(KodiZbritjesID))]
+        public virtual KodiZbritjes? KodiZbritjes { get; set; }
+        [ForeignKey(nameof(AdresaPorosisID))]
+        public virtual AdresatPerdoruesit? Adresa { get; set; }
+    }
+}
