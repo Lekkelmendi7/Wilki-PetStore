79fa0cd1b7c690e8d8cad46b4f79be64ddbd4174
Lekkelmendi7 <lek.kelmendi8@gmail.com>
2024-09-06 14:51:17 +0200

    Creating a controller for Product Photos

---
+++ Wilki/Controllers/API/Produktet/FototProduktitController.cs
@@ -0,0 +1,65 @@
+﻿using Wilki.Data;
+using Wilki.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Wilki.Controllers.API.Produktet
+{
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Route("api/Produktet/[controller]")]
+    [ApiController]
+    public class FototProduktitController : ControllerBase
+    {
+        private readonly IMongoCollection<FototProduktit>? _fototProduktit;
+        public FototProduktitController(MongoDBService mongoDBService)
+        {
+            _fototProduktit = mongoDBService.Database?.GetCollection<FototProduktit>("fotoProduktit");
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        [Route("ShfaqFototEProduktitPerGallery")]
+        public async Task<IActionResult> ShfaqFototEProduktit(int produktiId)
+        {
+            var filter = Builders<FototProduktit>.Filter.Eq(x => x.ProduktiID, produktiId);
+            var fotoProduktit = await _fototProduktit.Find(filter).ToListAsync();
+
+            return fotoProduktit is not null ? Ok(fotoProduktit) : NotFound();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("VendosFotonEProduktitPerGallery")]
+        public async Task<IActionResult> VendosFotonEProduktitPerGallery(FototProduktit foto)
+        {
+            await _fototProduktit.InsertOneAsync(foto);
+            return CreatedAtAction(nameof(ShfaqFototEProduktit), new { id = foto.Id }, foto);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete]
+        [Route("FshijFotonEProduktitPerGallery")]
+        public async Task<ActionResult> FshijFotonEProduktitPerGallery(string id)
+        {
+            var filter = Builders<FototProduktit>.Filter.Eq(x => x.Id, id);
+            var fotoProduktit = await _fototProduktit.Find(filter).FirstOrDefaultAsync();
+            await _fototProduktit.DeleteOneAsync(filter);
+
+            var follderi = Path.Combine("..", "wilkiweb", "public", "img", "produktet");
+
+            if (!fotoProduktit.EmriFotos.Equals("ProduktPaFoto.png"))
+            {
+                var fotoVjeter = Path.Combine(follderi, fotoProduktit.EmriFotos);
+
+                if (System.IO.File.Exists(fotoVjeter))
+                {
+                    System.IO.File.Delete(fotoVjeter);
+                }
+            }
+
+            return Ok("Foto u fshi me sukses!");
+        }
+    }
+}
