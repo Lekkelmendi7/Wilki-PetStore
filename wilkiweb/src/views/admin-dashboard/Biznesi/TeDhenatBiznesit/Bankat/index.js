6348874f45276fdab2465944d12feee3a3595b2a
eriDev1 <99198496+eriDev1@users.noreply.github.com>
2024-09-09 23:57:53 +0200

    Business page with its data created in admin dashboard.

---
+++ wilkiweb/src/views/admin-dashboard/Biznesi/TeDhenatBiznesit/Bankat/index.js
@@ -0,0 +1,149 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan, faPlus, faClose } from '@fortawesome/free-solid-svg-icons';
+import { TailSpin } from 'react-loader-spinner';
+import Tabela from '../../../../../components/Tabela/Tabela';
+import Mesazhi from '../../../../../components/Mesazhi';
+import { Helmet } from 'react-helmet';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import ShtoBanken from './ShtoBanken';
+import EditoBanken from './EditoBanken';
+import LargoBanken from './LargoBanken';
+import KontrolloAksesinNeFaqe from '../../../../../components/KontrolliAksesit/KontrolloAksesinNeFaqe';
+import Titulli from './../../../../../components/Titulli';
+
+function Bankat(props) {
+  const [klientet, setKlientet] = useState([]);
+  const [perditeso, setPerditeso] = useState('');
+  const [shfaqMesazhin, setShfaqMesazhin] = useState(false);
+  const [tipiMesazhit, setTipiMesazhit] = useState('');
+  const [pershkrimiMesazhit, setPershkrimiMesazhit] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const getToken = localStorage.getItem('token');
+
+  const authentikimi = {
+    headers: {
+      Authorization: `Bearer ${getToken}`
+    }
+  };
+
+  useEffect(() => {
+    const shfaqKlientet = async () => {
+      try {
+        setLoading(true);
+        const klientet = await axios.get('https://localhost:7251/api/Biznesi/TeDhenatBiznesit/ShfaqBankat', authentikimi);
+        setKlientet(klientet.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
+
+    shfaqKlientet();
+  }, [perditeso]);
+
+  const UsersData = klientet.map((k) => ({
+    ID: k.bankaID,
+    'Emri Bankes': k.emriBankes,
+    'Numri i Llogaris': k.numriLlogaris,
+    Adresa: k.adresaBankes,
+    Valuta: k.valuta
+  }));
+
+  const [shto, setShto] = useState(false);
+  const [edito, setEdito] = useState(false);
+  const [fshij, setFshij] = useState(false);
+  const [id, setId] = useState(0);
+
+  const handleClose = () => {
+    setShto(false);
+  };
+  const handleShow = () => setShto(true);
+
+  const handleEdito = (id) => {
+    console.log('id', id);
+    setEdito(true);
+    setId(id);
+  };
+  const handleEditoMbyll = () => setEdito(false);
+
+  const handleFshij = (id) => {
+    setFshij(true);
+    setId(id);
+  };
+  const handleFshijMbyll = () => setFshij(false);
+
+  return (
+    <div>
+      <KontrolloAksesinNeFaqe vetemAdmin/>
+      <Titulli titulli={'Bankat'} />
+      {shfaqMesazhin && <Mesazhi setShfaqMesazhin={setShfaqMesazhin} pershkrimi={pershkrimiMesazhit} tipi={tipiMesazhit} />}
+      {shto && (
+        <ShtoBanken
+          shfaq={handleShow}
+          largo={handleClose}
+          shfaqmesazhin={() => setShfaqMesazhin(true)}
+          perditesoTeDhenat={() => setPerditeso(Date.now())}
+          setTipiMesazhit={setTipiMesazhit}
+          setPershkrimiMesazhit={setPershkrimiMesazhit}
+        />
+      )}
+      {shfaqMesazhin && <Mesazhi setShfaqMesazhin={setShfaqMesazhin} pershkrimi={pershkrimiMesazhit} tipi={tipiMesazhit} />}
+      {edito && (
+        <EditoBanken
+          largo={handleEditoMbyll}
+          id={id}
+          shfaqmesazhin={() => setShfaqMesazhin(true)}
+          perditesoTeDhenat={() => setPerditeso(Date.now())}
+          setTipiMesazhit={setTipiMesazhit}
+          setPershkrimiMesazhit={setPershkrimiMesazhit}
+        />
+      )}
+      {fshij && (
+        <LargoBanken
+          largo={handleFshijMbyll}
+          id={id}
+          shfaqmesazhin={() => setShfaqMesazhin(true)}
+          perditesoTeDhenat={() => setPerditeso(Date.now())}
+          setTipiMesazhit={setTipiMesazhit}
+          setPershkrimiMesazhit={setPershkrimiMesazhit}
+        />
+      )}
+      {loading ? (
+        <div className="Loader">
+          <TailSpin
+            height="80"
+            width="80"
+            color="#009879"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          {UsersData.length > 0 ? (
+            <Tabela
+              data={UsersData}
+              tableName="Bankat"
+              kaButona
+              funksionButonEdit={(e) => handleEdito(e)}
+              funksionButonShto={() => handleShow()}
+              funksionButonFshij={(e) => handleFshij(e)}
+            />
+          ) : (
+            'Nuk ka te Dhena'
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Bankat;
