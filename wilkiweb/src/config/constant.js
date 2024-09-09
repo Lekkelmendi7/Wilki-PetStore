5785d5cd071cd1480bf65fba2057663efa290113
ahmetb00 <121351827+ahmetb00@users.noreply.github.com>
2024-09-09 10:51:44 +0200

    Creating css styles for DataTable, Footer Mesazhi and produktet

---
+++ wilkiweb/src/config/constant.js
@@ -0,0 +1,9 @@
+export const BASE_URL = '/dashboard';
+export const BASE_TITLE = ' | Wilki';
+export const PROFILI = 'https://localhost:7251/Identity/Account/Manage';
+
+export const CONFIG = {
+  layout: 'vertical',
+  collapseMenu: false,
+  layoutType: 'menu-dark'
+};
